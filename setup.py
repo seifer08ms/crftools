9b6c9762f21856747a18bb960143570e3ab2cb9b
seifer08ms <hi181904665@gmail.com>
2018-04-26 10:24:31 +0800

    add url, update version 0.0.3.5

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='crftools',
-    version='0.0.3.4',
+    version='0.0.3.5',
     keywords='crf predict check',
     description='useful for CRF++',
     license='MIT License',
     author='Chenliang Wang',
+    url='https://github.com/seifer08ms/crftools',
     author_email='chenliangwang08ms@gmail.com',
     packages=find_packages(),
     include_package_data=True,
